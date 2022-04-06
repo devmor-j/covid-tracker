1de16be93816f5b134395c3d1a9c15f5f768e514
Morteza Jamshidi <devmor.j@gmail.com>
2022-04-07 00:02:04 +0430

    Rename virus icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="w-full text-center text-gray-100 bg-blue-800 p-6 space-y-3">
     <div class="flex items-center justify-center gap-1">
-      <Icon class="w-8" path="./src/assets/svg/virus.svg" />
+      <Icon class="w-8" path="./src/assets/svg/covid.svg" />
       <h1 class="text-3xl md:text-4xl font-bold">
         <router-link to="/">Covid-19 Tracker</router-link>
       </h1>
